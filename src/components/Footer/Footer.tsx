c0725f73335f0363711332007391f38096c55a7e
Salvatore Calo <dafder45678@gmail.com>
2025-04-13 20:18:57 +0200

    fix: changed link of run polito in the footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
     return (
         <footer>
             <h3>Polito Appunti è un progetto <a href="https://github.com/salvatorecalo/polito_appunti" target='_blank'>Open Source</a></h3>
-            <h3>Powered by <a href="runpolito.it" target='_blank'>Run Polito</a></h3>
+            <h3>Powered by <a href="https://www.runpolito.it" target='_blank'>Run Polito</a></h3>
         </footer>
     );
 }
