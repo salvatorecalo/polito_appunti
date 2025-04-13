8b22f4f33606b7f7dad2fb81f468053b88e4b29f
Salvatore Calo <dafder45678@gmail.com>
2025-04-13 20:17:43 +0200

    fix: fix layout display

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,16 +1,16 @@
 import { Outlet } from "react-router";
 import { Navbar } from "../Navbar/Navbar";
 import { Footer } from "../Footer/Footer";
-
+import './Layout.css';
 
 export function Layout() {
     return (
-        <>
-            <section style={{margin: "2em 1em"}}>
+        <main className="app">
+            <section>
                 <Navbar />
                 <Outlet />
             </section>
             <Footer />
-        </>
+        </main>
     );
 }
