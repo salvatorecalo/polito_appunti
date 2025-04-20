4b288d2e94a4ce11fdbc27b577d84b0c00294348
Salvatore Calo <dafder45678@gmail.com>
2025-04-20 17:59:14 +0200

    fix(routin system): changed routing system. Now it's based on url params

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,20 +1,45 @@
-import { CategoryList, SearchPanel, UploadPanel, GeneralMaterial } from "../../components";
-import './HomePage.css'
+// pages/HomePage.tsx
+import { useSearchParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { getCategories } from '../../utils';
+import { CategoryList, SearchPanel, UploadPanel, GeneralMaterial } from '../../components';
+import { CategoryPage } from '../CategoryPage/CategoryPage';
+import './HomePage.css';
 
 export function HomePage() {
+  const [params] = useSearchParams();
+  const id = params.get('id');
+  const [label, setLabel] = useState<string | null>(null);
 
-    return (
-        <main>
-            <section id="HomePage">
-                <section className="action-container">
-                    <SearchPanel />
-                    <UploadPanel />
-                </section>
-                <section className="action-container">
-                    <GeneralMaterial />
-                    <CategoryList />
-                </section>
-            </section>
-        </main>
-    );
+  async function fetchCategory() {
+    if (id) {
+      const categories = await getCategories();
+      setLabel(categories[id] || null);
+    } else {
+      setLabel(null);
+    }
+  }
+  
+  useEffect(() => {
+    fetchCategory();
+  }, [id]);
+
+  if (label) {
+    return <CategoryPage categoryKey={id!} label={label} />;
+  }
+
+  return (
+    <main>
+      <section id="HomePage">
+        <section className="action-container">
+          <SearchPanel />
+          <UploadPanel />
+        </section>
+        <section className="action-container">
+          <GeneralMaterial />
+          <CategoryList />
+        </section>
+      </section>
+    </main>
+  );
 }
