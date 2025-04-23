f705ce55373e4ca527994a4b45277d7c48c776d8
Salvatore Calo <dafder45678@gmail.com>
2025-04-23 15:00:00 +0200

    fix: removed useless data from searchpage

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,11 @@
+import { FRONTEND_GITHUB_REPO, RUN_POLITO_WEBSITE } from '../../utils';
 import './Footer.css'
 
 export function Footer() {
     return (
         <footer>
-            <h3>Polito Appunti è un progetto <a href="https://github.com/salvatorecalo/polito_appunti" target='_blank'>Open Source</a></h3>
-            <h3>Powered by <a href="https://www.runpolito.it" target='_blank'>Run Polito</a></h3>
+            <h3>Polito Appunti è un progetto <a href={FRONTEND_GITHUB_REPO} target='_blank'>Open Source</a></h3>
+            <h3>Powered by <a href={RUN_POLITO_WEBSITE} target='_blank'>Run Polito</a></h3>
         </footer>
     );
 }
