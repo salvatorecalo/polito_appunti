7dc1e75a53729c575a4e1e84b33115cc7fdf262a
salvo <dafder45678@gmail.com>
2026-06-07 16:32:19 +0200

    fix: add force-dynamic to skip prerendering of upload page

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic"
+import { Suspense } from "react";
 import { getAppConfig } from "../server_actions/get_app_config/get_app_config";
 import UploadPage from "./page/uploadPage";
 
@@ -5,6 +7,8 @@ export default async function Page(){
     const {categories} = await getAppConfig();
     
     return (
-        <UploadPage courses={categories}/>
+        <Suspense fallback={<p style={{ textAlign: 'center', padding: '2rem' }}>Caricamento modulo...</p>}>
+            <UploadPage courses={categories}/>
+        </Suspense>
     )
 }
