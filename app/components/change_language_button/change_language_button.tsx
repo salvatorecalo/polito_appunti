64a29dfc783470bc2369afacc258b7309f4fa99a
salvo <dafder45678@gmail.com>
2026-06-13 23:33:01 +0200

    fix: make change language button more beautiful

--- app/components/change_language_button/change_language_button.tsx
+++ app/components/change_language_button/change_language_button.tsx
@@ -1,9 +1,22 @@
 "use client"
 import { useTranslation } from "@/app/(utils)/context/language_context/language_context"
+import './change_language_button.css'
 
-export function ChangeLanguageButton(){
-    const {lang, setLang} = useTranslation()
+export function ChangeLanguageButton() {
+    const { lang, setLang } = useTranslation()
+    
     return (
-        <button onClick={() => setLang(lang === "it" ? "en" : "it")}>{lang}</button>
+        <div className="switch">
+            <input 
+                id="language-toggle" 
+                className="check-toggle check-toggle-round-flat" 
+                type="checkbox" 
+                checked={lang === "en"}
+                onChange={() => setLang(lang === "it" ? "en" : "it")} 
+            />
+            <label htmlFor="language-toggle"></label>
+            <span className="on">IT</span>
+            <span className="off">EN</span>
+        </div>
     )
 }
