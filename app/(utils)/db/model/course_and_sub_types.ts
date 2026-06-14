3948fed738c64fc6a333cad8ff78c5c4148706ff
salvo <dafder45678@gmail.com>
2026-06-14 21:30:50 +0200

    feat: - add bachelor mechanical engineering category and set it language in the default state

--- app/(utils)/db/model/course_and_sub_types.ts
+++ app/(utils)/db/model/course_and_sub_types.ts
@@ -1,6 +1,6 @@
 export type CategoryKey = 
     | "dummy" | "ing" | "1st" | "2nd" | "lib" 
-    | "aer" | "bio" | "cin" | "elt" | "fis" | "ges" | "inf" | "mat" 
+    | "aer" | "bio" | "cin" | "elt" | "fis" | "ges" | "inf" | "mat" | "mech"
     | "biomag" | "eltmag" | "enrmag" | "gesmag" | "nfimag" 
     | string;
 
